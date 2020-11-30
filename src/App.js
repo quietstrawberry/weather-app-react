827ff5fd8266fc12f77a22dd21f5cd742a7300af
quietstrawberry <40902988+quietstrawberry@users.noreply.github.com>
2020-11-30 16:17:48 -0500

    Migrated sandbox to VS Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import Search from "./Search";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div>
+        <Search />
+        <br />
+        <h3 className="conditions">conditions | </h3>
+        <h3 className="conditions"> humidity | </h3>
+        <h3 className="conditions"> wind speed </h3>
+        <br />
+        <a href="https://github.com/quietstrawberry/World-Weather-Project">
+          My GitHub
         </a>
-      </header>
+        .
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Search.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function Search(props) {
+  const [city, setCity] = useState("");
+  const [display, setDisplay] = useState("Charlotte");
+  const [temperature, setTemperature] = useState(24);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setDisplay(city);
+    setTemperature("18");
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div>
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Type a city.."
+          autoComplete="off"
+          onChange={changeCity}
+        />
+        <input type="submit" placeholder="submit" />
+      </form>
+      <h1>{display}</h1>
+      <img className="icon" className="temp" src="/icon.png" width="80" />
+      <h2 className="temp">{temperature}° C|F</h2>
+    </div>
+  );
+}
