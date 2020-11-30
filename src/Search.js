7aacdc90121fc823ff2a1c04d4c842ef8d6abc5e
quietstrawberry <40902988+quietstrawberry@users.noreply.github.com>
2020-11-30 17:11:51 -0500

    addressed error warnings

--- src/Search.js
+++ src/Search.js
@@ -28,7 +28,7 @@ export default function Search(props) {
         <input type="submit" placeholder="submit" />
       </form>
       <h1>{display}</h1>
-      <img className="temp" src="/icon.png" width="80" alt-text="icon" />
+      <img className="temp" src="/icon.png" width="80" alt="icon" />
       <h2 className="temp">{temperature}° C|F</h2>
     </div>
   );
