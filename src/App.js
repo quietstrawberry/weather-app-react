b92de065c2ca6cdf067a2af844575891f193e54b
quietstrawberry <40902988+quietstrawberry@users.noreply.github.com>
2020-12-08 12:10:11 -0500

    Integrated OpenWeather API

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ export default function App() {
     <div className="App">
       <div>
         <Search />
-        <br />
-        <h3 className="conditions">conditions | </h3>
-        <h3 className="conditions"> humidity | </h3>
-        <h3 className="conditions"> wind speed </h3>
-        <br />
+        
         <a href="https://github.com/quietstrawberry/World-Weather-Project">
           My GitHub
         </a>
--- src/Search.js
+++ src/Search.js
@@ -1,35 +1,64 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "./styles.css";
+import axios from "axios";
+
 
 export default function Search(props) {
-  const [city, setCity] = useState("");
-  const [display, setDisplay] = useState("Charlotte");
-  const [temperature, setTemperature] = useState(24);
+    
+    const [weatherData, setWeatherData]=useState({ready:false});
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    setDisplay(city);
-    setTemperature("18");
-  }
 
-  function changeCity(event) {
-    setCity(event.target.value);
-  }
+    
 
-  return (
+function handleResponse(response){
+    console.log(response.data);
+    
+  setWeatherData({
+      ready:true,
+      temperature:response.data.main.temp,
+      humidity:response.data.main.humidity,
+      windSpeed:response.data.wind.speed,
+      conditions:response.data.weather[0].description,
+      city:response.data.name,
+      icon:"https://openweathermap.org/img/wn/10d@2x.png"
+  })
+
+   
+}
+
+
+  if (weatherData.ready) {return (
     <div>
-      <form className="search" onSubmit={handleSubmit}>
+      <form className="search" >
         <input
           type="search"
           placeholder="Type a city.."
           autoComplete="off"
-          onChange={changeCity}
+          
         />
         <input type="submit" placeholder="submit" />
       </form>
-      <h1>{display}</h1>
-      <img className="temp" src="/icon.png" width="80" alt="icon" />
-      <h2 className="temp">{temperature}° C|F</h2>
+      <h1>{weatherData.city}</h1>
+      <img className="temp" src= {weatherData.icon} width="80" alt={weatherData.description} />
+      <h2 className="temp">{Math.round(weatherData.temperature)}° C|F</h2>
+    
+    <br />
+    <h3 className="conditions">{weatherData.conditions} | </h3>
+    <h3 className="conditions"> Humidity: {weatherData.humidity}% | </h3>
+    <h3 className="conditions"> Wind Speed: {Math.round(weatherData.windSpeed)} km/h</h3>
+    <br />
     </div>
   );
+  }
+ else    {
+const apiKey="04e33782c3a1504fe52423621fcf24dd";
+let city="Charlotte";
+let units="metric";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+axios.get(apiUrl).then(handleResponse);
+
+return ("Loading...");
+}
+
+
 }
